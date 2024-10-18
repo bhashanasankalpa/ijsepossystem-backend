87795a2f9c2b226d725849756f0dd0702f75670d
bhashanasankalpa <bhashanasankalpa@gmail.com>
2024-10-18 16:18:20 +0530

    Initialize Backend and Connect MySQL

---
+++ src/main/java/com/ijse/pos_assignment/PosAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.pos_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijse/pos_assignment/PosAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.pos_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
